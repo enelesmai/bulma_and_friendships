bc23a1cdedcc88cc14abe9acc4ea20a82e10ac92
enelesmai <xochitl.selene.fp@gmail.com>
2020-05-05 11:53:57 -0500

    added email list

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
  
+  def friend_emails
+    friends.map{|e| e.email}
+  end
+
 end
